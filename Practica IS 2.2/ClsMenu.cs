e92962a7b9907fd59dfef983aea7f3a0a00abdbf
Néstor Martínez Hernandez <nestormartinezh@outlook.com>
2017-06-07 19:37:03 -0500

    Inicio

---
+++ Practica IS 2.2/ClsMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_IS_2._2
+{
+    public class ClsMenu
+    {
+        //int A = 270, B = 340, C = 390;
+
+        public void MostrarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                "Producto A $270\n" +
+                "Producto B $340\n" +
+                "Producto C $390\n\n" +
+                "Seleccione un producto");
+                return;
+        }   
+        public void Cobrar(int precio)
+        {
+            Console.WriteLine("Introdusca cantidad: ");
+
+            int saldo = Console.Read();
+
+            if (saldo != 0)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ Practica IS 2.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_IS_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsMenu menu = new ClsMenu();
+
+            menu.MostrarMenu();
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "a":
+                    menu.Cobrar(270);
+                    break;
+                case "b":
+                    menu.Cobrar(230);
+                    break;
+                case "c":
+                    menu.Cobrar(290);
+                    break;
+                default:
+                    Console.WriteLine(opcion + " No es una opcion valida. pulse enter para continuar");
+                    Console.ReadLine();
+                    menu.MostrarMenu();
+                    break;
+            }
+
+        }
+    }
+}
